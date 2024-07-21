cfd8a55dea8506e2f787d7e13c74621b550acc13
Mustafa Siddiqui <mustafa.a.r.siddiqui@outlook.com>
2024-07-21 01:14:10 -0400

    task/ fix multipart detection

--- src/http_parser.cpp
+++ src/http_parser.cpp
@@ -33,19 +33,20 @@ namespace rfss {
         line_stream >> req.method >> req.URI >> req.version;
 
         while (std::getline(iss, line) && line != "\r") {
+            if (line.back() == '\r') {
+                line.pop_back(); 
+            }
             size_t pos = line.find(':');
             if (pos != std::string::npos) {
                 std::string key = line.substr(0, pos);
                 std::string value = line.substr(pos + 1);
 
-                // Trim leading and trailing spaces from key and value
                 key.erase(0, key.find_first_not_of(" \t"));
                 key.erase(key.find_last_not_of(" \t") + 1);
                 value.erase(0, value.find_first_not_of(" \t"));
                 value.erase(value.find_last_not_of(" \t") + 1);
 
                 if (key == "Cookie") {
-                    // Parse cookies
                     std::istringstream cookie_stream(value);
                     std::string cookie_pair;
                     while (std::getline(cookie_stream, cookie_pair, ';')) {
@@ -57,71 +58,73 @@ namespace rfss {
                         }
                     }
                 } else {
-                    // Add other headers
                     req.headers.emplace_back(key, value);
                 }
             }
         }
+
         for (const auto& header : req.headers) {
             if (header.first == "Content-Length") {
                 int content_length = std::stoi(header.second);
                 if (content_length > 0) {
                     std::string body_content(content_length, '\0');
-                    if (iss.read(&body_content[0], content_length)) {
-                        
-                        auto content_type_header = std::find_if(req.headers.begin(), req.headers.end(), 
-                            [](const std::pair<std::string, std::string>& header) {
-                                return header.first == "Content-Type";
-                            });
+                    iss.read(&body_content[0], content_length);
 
-                        if (content_type_header != req.headers.end()) 
+                    bool is_form_data = false;
+                    bool is_multipart_data = false;
+                    for (const auto& header : req.headers) {
+                        if (header.first == "Content-Type") {
+                            if (header.second.find("application/x-www-form-urlencoded") != std::string::npos) {
+                                is_form_data = true;
+                            } else if (header.second.find("multipart/form-data") != std::string::npos) {
+                                is_multipart_data = true;
+                            }
+                        }
+                    }
 
-                            if (content_type_header->second.find("application/x-www-form-urlencoded") != std::string::npos) 
-                                parse_form_data(body_content, req);
-
-                            else if (content_type_header->second.find("multipart/form-data") != std::string::npos) 
-                                std::cout << "Encountered form data\n";
-
-                            else 
-                                req.body = body_content;
-                            
-                    } else 
-                        std::cerr << "Could not read body content" << std::endl;
+                    if (is_form_data) {
+                        parse_form_data(body_content, req);
+                    } else if (is_multipart_data) {
+                        std::cout << "Multipart data encountered" << std::endl;
+                    } else {
+                        req.body = body_content;
+                    }
                 }
-                break;
+                break; 
             }
         }
     }
 
 
+
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~ Recieve Request ~~~~~~~~~~~~~~~~~~~~~~~~~~
     void handle_client(int client_socket) {
-    char buffer[BUFFER_SIZE];
-    std::string http_request_string;
-    int bytes_read = 0;
+        char buffer[BUFFER_SIZE];
+        std::string http_request_string;
+        int bytes_read = 0;
 
-    do {
-        bytes_read = recv(client_socket, buffer, BUFFER_SIZE, 0);
-        if (bytes_read < 0) {
-            std::cerr << "Error: Client disconnected from server or no data received!\n";
-            close(client_socket);
-            return;
-        }
-        if (bytes_read == 0)
-            break;
+        do {
+            bytes_read = recv(client_socket, buffer, BUFFER_SIZE, 0);
+            if (bytes_read < 0) {
+                std::cerr << "Error: Client disconnected from server or no data received!\n";
+                close(client_socket);
+                return;
+            }
+            if (bytes_read == 0)
+                break;
 
-        http_request_string.append(buffer,bytes_read);
+            http_request_string.append(buffer,bytes_read);
 
-        if (http_request_string.find("\r\n\r\n") != std::string::npos)
-            break;
-    
-    } while (bytes_read > 0);
+            if (http_request_string.find("\r\n\r\n") != std::string::npos)
+                break;
+        
+        } while (bytes_read > 0);
 
 
-    HTTPRequest request;
-    parse_request(request, http_request_string);
-    handle_request(request, client_socket);
-    close(client_socket);
-}
+        HTTPRequest request;
+        parse_request(request, http_request_string);
+        handle_request(request, client_socket);
+        close(client_socket);
+    }
 
 }
